6ff4aa9484d540465eab87fe50324820f181f730
Jaime Pascual Torres <jpascual@everlyrusher.com>
2024-04-10 13:21:32 +0200

    Recipe Filter by API

--- tests/Feature/RecipeTest.php
+++ tests/Feature/RecipeTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\Recipe;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class RecipeTest extends TestCase
@@ -23,5 +22,38 @@ class RecipeTest extends TestCase
         $this->assertNotEmpty($recipe->body);
 
         $this->assertDatabaseHas('recipes', $recipe->toArray());
+    }
+
+    /**
+     * Recipe filtered search by text.
+     */
+    public function test_recipe_search_endpoint(): void
+    {
+        $recipe_to_search = Recipe::factory()->create([
+            'title' => 'Recipe 1',
+            'body' => 'Recipe 1 body',
+        ]);
+
+        Recipe::factory(2)->create([
+            'title' => 'Recipe not 1',
+            'body' => 'Recipe not 1 body',
+        ]);
+
+        $this
+            ->getJson( route(
+                'recipe.filter',
+                ['text' => $recipe_to_search->title],
+            ))
+            ->assertStatus(200)
+            ->assertJsonCount(1)
+            ->assertJsonStructure([
+                '*' => [
+                    'id',
+                    'title',
+                    'body',
+                    'created_at',
+                    'updated_at',
+                ],
+            ]);
     }
 }
