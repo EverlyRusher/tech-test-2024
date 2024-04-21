b51ffafa8e4ed6e6079a155b00b2ff8b8b60112a
vinoles <felipe.vinoles@gmail.com>
2024-04-22 01:26:52 +0200

    remove comments in web routes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\RetrieveRecipes;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', static function () {
-//     return view('home');
-// });
-
 Route::get('/', RetrieveRecipes::class)->name('home');
